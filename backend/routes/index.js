5523b3ca86a96b4c3ad449a7334d602587671146
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-09-19 19:13:15 -0400

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,19 @@
 const express = require('express');
 const router = express.Router();
 
+// Import the api router
+const apiRouter = require('./api');
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-Token': csrfToken
+    });
 });
+
+// Use the api router for all routes starting with /api
+router.use('/api', apiRouter);
 
 module.exports = router;
