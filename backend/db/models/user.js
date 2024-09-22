8d238b669f8ead5c71a5387ab63654e0ac31cdf4
richard <rslrockon@gmail.com>
2024-09-22 16:14:20 -0700

    Add User model scope for user security

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,19 +1,32 @@
-'use strict';
+/*'use strict';
 const {
   Model
 } = require('sequelize');
 const { validator } = require('sequelize/lib/utils/validator-extras');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
+    
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
-     */
+     
     static associate(models) {
       // define association here
     }
   }
+*/
+
+'use strict';
+
+const { Model, Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+
   User.init({
     username: {
       type: DataTypes.STRING,
