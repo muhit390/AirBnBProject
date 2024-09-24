1068aedf55b7353d1650fd4082406b57ba29074a
richard <rslrockon@gmail.com>
2024-09-24 11:52:34 -0700

    predeploy

--- backend/db/migrations/20240922223943-create-user.js
+++ backend/db/migrations/20240922223943-create-user.js
@@ -47,8 +47,8 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       }
-    });
-    options;
+    }, options);
+   
   },
   async down(queryInterface, Sequelize) {
     //await queryInterface.dropTable('Users');
