9f1d040540603133d574e508a4e36e956f2c93be
richard <rslrockon@gmail.com>
2024-09-25 12:57:44 -0700

    options fixed

--- backend/db/migrations/20240925053530-create-reviews.js
+++ backend/db/migrations/20240925053530-create-reviews.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const { all } = require('../../routes/api/session');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA
+};
+
 module.exports = {
     async up(queryInterface, Sequelize) {
         await queryInterface.createTable('Reviews', {
@@ -48,9 +55,10 @@ module.exports = {
                 type: Sequelize.DATE,
                 defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
             }
-        });
+        }, options);
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable('Reviews');
+        options.tableName = "Reviews"
+        return queryInterface.dropTable(options);
     }
 };
