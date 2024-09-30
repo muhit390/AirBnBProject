88b6a7c645e38c59fa827c226aa9131255f4abca
richard <rslrockon@gmail.com>
2024-09-30 15:40:49 -0700

    try again

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -16,12 +16,6 @@ module.exports = (sequelize, DataTypes) => {
         onDelete: 'CASCADE',
         hooks: true,
       });
-      //has many Bookings
-      User.hasMany(models.Booking, {
-        foreignKey: 'userId',
-        onDelete: 'CASCADE',
-        hooks: true,
-      });
       User.hasMany(models.Review, {
         foreignKey: 'userId',
         onDelete: 'CASCADE',
