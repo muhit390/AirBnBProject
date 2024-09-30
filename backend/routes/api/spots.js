55321dcf27ce6b664d46b6fb1e78d35d3b881e32
richard <rslrockon@gmail.com>
2024-09-30 14:32:23 -0700

    requireauth

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { requireAuth } = require('../../utils/auth');
 
 
 const validateSpots = [
