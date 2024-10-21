f20d9d3f6a23ede066da8013508c1b464f04cc0a
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-10-20 23:23:11 -0400

    WITH DIST FLODER

---
+++ frontend/dist/images/images.js
@@ -0,0 +1,25 @@
+// Import images
+import bathroom1 from './bathroom1.png';
+import bedroom2 from './bedroom2.png';
+import image3 from './image3.jpg';
+import image4 from './image4.jpg';
+import image5 from './image5.jpg';
+import image6 from './image6.jpg';
+import image7 from './image7.jpg';
+import kitchen2 from './kitchen2.png';
+import livingroom1 from './livingroom1.png';
+import livingroom2 from './livingroom2.png';
+
+// Export the images as an array or an object
+export const images = [
+    bathroom1,
+    bedroom2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    kitchen2,
+    livingroom1,
+    livingroom2
+];
